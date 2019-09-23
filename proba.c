0b5ec83986b7d8e65a25b78a236f1569ee85231b
MrBrenlla <47985849+MrBrenlla@users.noreply.github.com>
2019-09-23 14:22:55 +0200

    Add files via upload

---
+++ proba.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int acabado = 0;
+char  *teclado[];
+char  comando[];
+char  argumento[];
+int a,b;
+
+int TrocearCadena(char * cadena, char * trozos[]);
+
+void main() {
+	while (acabado != 1){
+		printf("->");
+		scanf("%c",teclado);
+		TrocearCadena()
+	}	
+}
+
+
+
+int TrocearCadena(char * cadena, char * trozos[])
+{ int i=1;
+if ((trozos[0]=strtok(cadena," \n\t"))==NULL)
+return 0;
+while ((trozos[i]=strtok(NULL," \n\t"))!=NULL)
+i++;
+return i;
+}
