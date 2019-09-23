1df9b0509f75c01e48d75de4d7a3f934fb67cf0b
Lorudarkuh <47794989+Lorudarkuh@users.noreply.github.com>
2019-09-23 19:05:27 +0200

    Update proba.c

--- proba.c
+++ proba.c
@@ -1,5 +1,22 @@
 /*
-LORUDARKUH EDITANDO O ARCHIVOH B)
+Struct nodo
+{
+Int dato;
+Struct nodo *enlace;
+};
+Struct nodo *cabecera;
+cabecera=NULL;
+
+-----------------------------------
+
+typedef struct Elemento
+{
+int dato;
+struct Elemento *enlace;
+Cabecera=NULL;
+} Nodo;
+Nodo*cabecera;
+*/
 
 #include <stdio.h>
 
@@ -7,20 +24,9 @@ int acabado = 0;
 char  *teclado[];
 char  comando[];
 char  argumento[];
-int a,b;
-
-int TrocearCadena(char * cadena, char * trozos[]);
-
-void main() {
-	while (acabado != 1){
-		printf("->");
-		scanf("%c",teclado);
-		TrocearCadena()
-	}	
-}
-
-
-
+/*
+--------------------------------------------------------------------------------
+*/
 int TrocearCadena(char * cadena, char * trozos[])
 { int i=1;
 if ((trozos[0]=strtok(cadena," \n\t"))==NULL)
@@ -29,4 +35,16 @@ while ((trozos[i]=strtok(NULL," \n\t"))!=NULL)
 i++;
 return i;
 }
+/*
+--------------------------------------------------------------------------------
+*/
+void main() {
+	while (acabado != 1){
+		printf("->");
+		scanf("%c", &teclado);
+		TrocearCadena();
+	}
+}
+/*
+--------------------------------------------------------------------------------
 */
