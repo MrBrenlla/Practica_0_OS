1805980e11fc8d6604453fa81474de8253ce688f
MrBrenlla <47985849+MrBrenlla@users.noreply.github.com>
2019-09-24 19:33:33 +0200

    Add files via upload

--- proba.c
+++ proba.c
@@ -22,15 +22,20 @@ Nodo*cabecera;
 #include <string.h>
 
 int acabado = 0;
+int numPalabras;
 char  teclado[101];
 char  comando[101];
+char argumento[101];
 /*
 --------------------------------------------------------------------------------
 */
-void TrocearCadena(char  cadena[], char  trozos[])
+int TrocearCadena(char  cadena[], char  com[] , char arg[])
 {
 	int espacios =0;
-	for (int i=0; i<100  ; i++){
+	int palabras =0;
+	int letras =0;
+	char trozos[101];
+	for (int i=0; cadena[i]!='\0'  ; i++){
 		if (cadena[i]==' '){
 			espacios+=1;
 		}
@@ -42,6 +47,19 @@ void TrocearCadena(char  cadena[], char  trozos[])
 			}
 		}
 	}
+	for (int i=0 ; i<101 ; i++ ) {
+		if (trozos[i]==' '){
+			palabras+=1;
+		}
+		else{
+			switch (palabras) {
+				case 0:com[i]=trozos[i]; letras+=1; break;
+				case 1:arg[i-(letras+1)]=trozos[i]; break;
+				default: return palabras; break;
+			}
+		}
+	}
+return palabras;
 }
 
 /*
@@ -60,12 +78,25 @@ void main() {
 	while (acabado != 1){
 		printf("->");
 		gets(teclado);
-		TrocearCadena(teclado , comando);
-		for (int i=0 ; i<101 ; i++){
-		printf("%c",  comando[i] );
-	}
+		//Devolbe o comando, o argumento e un número que corresponde a se hai 1, 2 ou mais palabras(3)
+		numPalabras=TrocearCadena(teclado , comando, argumento);
+
+		//mostrar por pantalla comando e argumento, para probas
+		for (int i=0 ; comando[i]!='\0' ; i++){
+			printf("%c",  comando[i] );
+		}
+		printf("\n" );
+		for (int i=0 ; argumento[i]!='\0' ; i++){
+			printf("%c",  argumento[i] );
+		}
+		printf("\n" );
+
+
+
+
 		limpiarBuffer(teclado);
 		limpiarBuffer(comando);
+		limpiarBuffer(argumento);
 
 
 	}
