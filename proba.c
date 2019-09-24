1e3643053feb6509883d9984157becc0c70e9e1b
MrBrenlla <47985849+MrBrenlla@users.noreply.github.com>
2019-09-24 16:27:22 +0200

    Add files via upload

--- proba.c
+++ proba.c
@@ -22,25 +22,23 @@ Nodo*cabecera;
 #include <string.h>
 
 int acabado = 0;
-char  teclado[];
-char  comando[];
+char  teclado[101];
+char  comando[101];
 /*
 --------------------------------------------------------------------------------
-*/
-
+*
 int TrocearCadena(char * cadena, char * trozos[]) {
 int i=1;
-
-if ((trozos[0]=strtok(cadena," \n\t"))==NULL)
+if ((trozos[0]=strtok(cadena," \n\t"))==NULL){
 	return 0;
- while ((trozos[i]=strtok(NULL," \n\t"))!=NULL)
+}
+ while ((trozos[i]=strtok(NULL," \n\t"))!=NULL){
  	i++;
+	}
 return i;
 }
-
-
-/*
-void TrocearCadena(char * cadena[], char * trozos[])
+*/
+void TrocearCadena(char  cadena[], char  trozos[])
 {
 	int espacios =0;
 	for (int i=0; i<100  ; i++){
@@ -49,11 +47,14 @@ void TrocearCadena(char * cadena[], char * trozos[])
 		}
 		else{
 			trozos[i-espacios]=cadena[i];
+			if( cadena[i+1]==' '){
+				trozos[(i-espacios)+1]=' ';
+				espacios-=1;
+			}
 		}
 	}
-
 }
-*/
+
 /*
 --------------------------------------------------------------------------------
 */
@@ -65,86 +66,12 @@ void TrocearCadena(char * cadena[], char * trozos[])
 void main() {
 	while (acabado != 1){
 		printf("->");
-		gets(&teclado);
-		TrocearCadena(&teclado , &comando);
-		printf("%s\n", comando[1] );
+		gets(teclado);
+		TrocearCadena(teclado , comando);
+		for (int i=0 ; i<101 ; i++){
+		printf("%c",  comando[i] );
 
 	}
-}
-/*
---------------------------------------------------------------------------------
-*/
-/*
-Struct nodo
-{
-Int dato;
-Struct nodo *enlace;
-};
-Struct nodo *cabecera;
-cabecera=NULL;
-
------------------------------------
-
-typedef struct Elemento
-{
-int dato;
-struct Elemento *enlace;
-Cabecera=NULL;
-} Nodo;
-Nodo*cabecera;
-*/
-
-#include <stdio.h>
-#include <string.h>
-
-int acabado = 0;
-char  teclado[];
-char  comando[];
-/*
---------------------------------------------------------------------------------
-*/
-
-int TrocearCadena(char * cadena, char * trozos[]) {
-int i=1;
-
-if ((trozos[0]=strtok(cadena," \n\t"))==NULL)
-	return 0;
- while ((trozos[i]=strtok(NULL," \n\t"))!=NULL)
- 	i++;
-return i;
-}
-
-
-/*
-void TrocearCadena(char * cadena[], char * trozos[])
-{
-	int espacios =0;
-	for (int i=0; i<100  ; i++){
-		if (cadena[i]==' '){
-			espacios+=1;
-		}
-		else{
-			trozos[i-espacios]=cadena[i];
-		}
-	}
-
-}
-*/
-/*
---------------------------------------------------------------------------------
-*/
-//int detectarComando ()
-
-/*
---------------------------------------------------------------------------------
-*/
-void main() {
-	while (acabado != 1){
-		printf("->");
-		gets(&teclado);
-		TrocearCadena(&teclado , &comando);
-		printf("%s\n", comando[1] );
-
 	}
 }
 /*
